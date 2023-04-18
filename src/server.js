850946de0540d391c996b63a34f7142c2d9cc9c1
damien lewis <damien_lewis2k12@hotmail.com>
2023-04-18 14:10:07 +0100

    add start and dev

--- src/server.js
+++ src/server.js
@@ -3,10 +3,11 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "App is Healthy"});
 });
-
 
 app.listen(5001, () => {
     console.log("server is listening");
