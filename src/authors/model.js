d54b6889cc104bb1fa0eb7f48c901e637a9178f2
damien lewis <damien_lewis2k12@hotmail.com>
2023-04-20 16:56:59 +0100

    fixed issue

--- src/authors/model.js
+++ src/authors/model.js
@@ -4,7 +4,8 @@ const connection = require("../db/connection");
 const Author = connection.define("Author", {
     authorName: {
         type: DataTypes.STRING,
-
+        allowNull: false,
+        unique: true,
     },
 }, {timestamps: false},
   {
--- src/genres/model.js
+++ src/genres/model.js
@@ -4,7 +4,8 @@ const connection = require("../db/connection");
 const Genre = connection.define("Genre", {
     genreName: {
         type: DataTypes.STRING,
-
+        allowNull: false,
+        unique: true,
     },
 }, {timestamps: false},
   {
